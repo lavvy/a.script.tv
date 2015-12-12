efcbbf58d3c5134f5d6c51a88b4f87c4adb6b650
lavvy <lavashonline@gmail.com>
2015-12-12 06:02:54 -0500

    latest tested

--- install.sh
+++ install.sh
@@ -1,33 +1,50 @@
 #!/bin/sh
+
 #
+
 # File: install.sh
+
 # Author: pip lavvy
+
 # Purpose: This script will configure a a. Script. Tv
+
+
+
 
 
 #replace skin
 
-#sudo rm /usr/share/kodi/addons/skin.osmc
 
-if [[ -d /usr/share/kodi/addons/skin.osmc.lock ]]; then
+
+#sudo chmod 777 /usr/share/kodi/addons
+
+if [ -d /usr/share/kodi/addons/skin.osmc.lock ]; then
     echo "Removing Lock"
     rm -rf /usr/share/kodi/addons/skin.osmc.lock
 fi
 
+#sudo rm -r /usr/share/kodi/addons/skin.osmc
 
-sudo cp assets/addons /usr/share/kodi
+if [ -f /usr/splash_sad.png ]; then
+    echo "Removing file"
+    rm -rf /usr/splash_sad.png
+fi
+
+
+if [ -f /usr/splash.png ]; then
+    echo "Removing file"
+    rm -rf /usr/splash.png
+fi
+#sudo rm -rf /usr/splash_sad.png
+
+#sudo rm -rf /usr/splash.png
 
 
 
-#replace splash pics
 
-sudo rm /usr/splash.png
 
 sudo cp assets/addons/splash.png /usr/splash.png
 
-
-sudo rm /usr/splash_sad.png
-
 sudo cp assets/addons/splash_sad.png /usr/splash_sad.png
 
-
+sudo cp -a assets/addons /usr/share/kodi/addons
